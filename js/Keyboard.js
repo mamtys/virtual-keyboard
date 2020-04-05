e8c2fbb247d1d30a4a2a3ed85ce0500f55c237c6
mamtys <Mamtysman@gmail.com>
2020-04-05 14:27:35 +0300

    feat: add output to textarea

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -1,50 +1,92 @@
 import Component from './lib/Component.js';
-import KeySwitcher from './KeySwitcher.js';
+import KeyCode from './KeyCode.js';
 
 import pick from './helpers/pick.js';
+
 
 class Keyboard extends Component {
   constructor(codeTable, languages, currentLanguage) {
     super('<div class="virtual-keyboard__keyboard keyboard"></div>');
-    this.switchersMap = null;
+    this.keyCodeMap = null;
 
-    this.initSwitchable(codeTable, languages, currentLanguage);
+    this.initKeyCodeMap(codeTable, languages, currentLanguage);
     this.appendChildren();
   }
 
-  initSwitchable(
+  initKeyCodeMap(
     codeTable,
     languages,
     currentLanguage,
   ) {
     const map = new Map();
-
     codeTable.forEach((languageTable, code) => {
+      if (languageTable.isFunctional) {
+        map.set(code, new KeyCode(code, languageTable, currentLanguage));
+        return;
+      }
       const filteredTable = pick(languageTable, languages);
-      map.set(code, new KeySwitcher(code, filteredTable, currentLanguage));
+      map.set(code, new KeyCode(code, filteredTable, currentLanguage));
     });
 
-    this.switchersMap = map;
+    this.keyCodeMap = map;
   }
 
   appendChildren() {
-    this.switchersMap.forEach((el) => this.appendComponent(el));
+    this.keyCodeMap.forEach((el) => this.appendComponent(el));
   }
 
   displayLanguage(language) {
-    this.switchersMap.forEach((el) => el.displayLanguage(language));
+    this.keyCodeMap.forEach((el) => el.displayLanguage(language));
   }
 
   displayAuxiliaryKey(keyCode) {
-    this.switchersMap.forEach((el) => el.displayAuxiliaryKey(keyCode));
+    this.keyCodeMap.forEach((el) => el.displayAuxiliaryKey(keyCode));
   }
 
   pressKey(keyCode) {
-    this.switchersMap.get(keyCode).pressKey();
+    try {
+      const codeObj = this.keyCodeMap.get(keyCode);
+      codeObj.pressKey();
+      if (codeObj.isFunctional) {
+        return;
+      }
+
+      const newPos = this.insertAfterCursor(codeObj.getCurrentValue());
+      this.fixCursorPosition(newPos);
+    } catch (err) {
+      console.error(`button ${keyCode} not implemented`, err);
+    }
+  }
+
+  insertAfterCursor(value) {
+    const pos = this.output.selectionStart;
+
+    if (this.output.textContent.length === 0) {
+      this.output.textContent += value;
+      return pos + value.length;
+    }
+
+    const array = this.output.textContent.split('');
+    array.splice(pos, 0, value);
+
+    this.output.textContent = array.join('');
+
+    return pos + value.length;
+  }
+
+  fixCursorPosition(pos) {
+    this.output.selectionStart = pos;
+    this.output.selectionEnd = pos;
   }
 
   releaseKey(keyCode) {
-    this.switchersMap.get(keyCode).releaseKey();
+    this.keyCodeMap.get(keyCode).releaseKey();
+
+    const x = window.scrollX;
+    const y = window.scrollY;
+
+    this.output.focus();
+    window.scrollTo(x, y);
   }
 }
 
