bf1bcb6c0d78d53e357bbe6c9a66854cf2881642
mamtys <Mamtysman@gmail.com>
2020-04-05 14:24:16 +0300

    feat: add autofocuc

--- js/Textarea.js
+++ js/Textarea.js
@@ -2,7 +2,7 @@ import Component from './lib/Component.js';
 
 class Textarea extends Component {
   constructor() {
-    super('<textarea class="virtual-keyboard__textarea textarea"></textarea>');
+    super('<textarea class="virtual-keyboard__textarea textarea" autofocus></textarea>');
   }
 }
 
