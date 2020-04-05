33fd1c6fe499518d4a538a4381df008dfb0c9edc
mamtys <Mamtysman@gmail.com>
2020-04-05 20:34:40 +0300

    fix: fix cursor position

--- js/helpers/insertAfterCursor.js
+++ js/helpers/insertAfterCursor.js
@@ -1,5 +1,11 @@
 function insertAfterCursor(input, value) {
   const pos = input.selectionStart;
+  console.log(input.textContent);
+  if (input.textContent.length === 0) {
+    input.textContent.concat(value);
+    console.log(input.textContent);
+    return;
+  }
 
   input.textContent
     .split('')
