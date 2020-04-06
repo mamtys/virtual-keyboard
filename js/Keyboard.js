4db7db6dda5c821d9628538146841c1c43af1a7f
mamtys <Mamtysman@gmail.com>
2020-04-06 21:29:03 +0300

    feat: set textarea control text work

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -3,7 +3,6 @@ import KeyCode from './KeyCode.js';
 
 import pick from './helpers/pick.js';
 
-
 class Keyboard extends Component {
   constructor(codeTable, languages, currentLanguage) {
     super('<div class="virtual-keyboard__keyboard keyboard"></div>');
@@ -51,42 +50,17 @@ class Keyboard extends Component {
         return;
       }
 
-      const newPos = this.insertAfterCursor(codeObj.getCurrentValue());
-      this.fixCursorPosition(newPos);
+      this.output.insert(codeObj.getCurrentValue());
     } catch (err) {
       console.error(`button ${keyCode} not implemented`, err);
     }
   }
 
-  insertAfterCursor(value) {
-    const pos = this.output.selectionStart;
 
-    if (this.output.textContent.length === 0) {
-      this.output.textContent += value;
-      return pos + value.length;
-    }
-
-    const array = this.output.textContent.split('');
-    array.splice(pos, 0, value);
-
-    this.output.textContent = array.join('');
-
-    return pos + value.length;
-  }
-
-  fixCursorPosition(pos) {
-    this.output.selectionStart = pos;
-    this.output.selectionEnd = pos;
-  }
 
   releaseKey(keyCode) {
     this.keyCodeMap.get(keyCode).releaseKey();
-
-    const x = window.scrollX;
-    const y = window.scrollY;
-
     this.output.focus();
-    window.scrollTo(x, y);
   }
 }
 
--- js/Textarea.js
+++ js/Textarea.js
@@ -1,8 +1,38 @@
-import Component from './lib/Component.js';
+import Component from '../../lib/Component';
+import getNextEndOfLine from '../../helpers/getNextEndOfLine';
+import getPreviousEndOfLine from '../../helpers/getPreviousEndOfLine';
 
 class Textarea extends Component {
   constructor() {
     super('<textarea class="virtual-keyboard__textarea textarea" autofocus></textarea>');
+  }
+
+  isNotSelected() {
+    return this.node.selectionStart === this.node.selectionEnd;
+  }
+
+  insert(value) {
+    const start = this.node.selectionStart;
+    const end = this.node.selectionEnd - start;
+
+    const array = this.node.textContent.split('');
+    array.splice(start, end, value);
+    this.node.textContent = array.join('');
+
+    this.fixCursorPosition(start + value.length);
+  }
+
+  fixCursorPosition(pos) {
+    this.node.selectionStart = pos;
+    this.node.selectionEnd = pos;
+  }
+
+  focus() {
+    const x = window.scrollX;
+    const y = window.scrollY;
+
+    this.node.focus();
+    window.scrollTo(x, y);
   }
 }
 
