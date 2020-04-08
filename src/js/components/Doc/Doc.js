9774ff52b2957a1177241a11ea130a2b1b6c6eea
mamtys <Mamtysman@gmail.com>
2020-04-08 18:31:23 +0300

    feat: update message
    notify users bug is fixed

--- src/js/components/Doc/Doc.js
+++ src/js/components/Doc/Doc.js
@@ -3,7 +3,8 @@ import Component from '../../lib/Component';
 class Doc extends Component {
     constructor() {
         super('<div class="virtual-keyboard__doc"></div>');
-        this.node.textContent = `Warning: I didn't noticed, layout is broken in chrome, use firefox to check functionslity
+        this.node.textContent = `Warning: I didn't notice, layout is broken in chrome, use firefox to check functionslity
+UPD: fixed chrome bug 08.04.20
 Keyboard was created at Win OS
 Lang Switch = Shift + LeftAlt`;
     } 
