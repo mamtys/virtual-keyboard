e9219007d3f6b70cb96927fa1219523f465de88b
mamtys <Mamtysman@gmail.com>
2020-04-08 19:18:59 +0300

    fix: fix layout for chrome

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 import VirtualKeyboard from './components/VirtualKeyboard/VirtualKeyboardContainer';
 import data from './config/KEYBOARD_EVENT_DATA';
 import LocalStorageManager from './helpers/LocalStorageManager';
+import Doc from './components/Doc';
 import defaults from './config/KEYBOARD_DEFAULTS';
 
 const config = LocalStorageManager.getUserConfig() || defaults;
 
 const virtualKeyboard = new VirtualKeyboard(data, config);
+const doc = new Doc();
 
+virtualKeyboard.appendComponent(doc);
 document.body.appendChild(virtualKeyboard.getNode());
 
 window.addEventListener('beforeunload', () => {
