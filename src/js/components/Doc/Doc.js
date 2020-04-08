affac2dd4b45a0830c250769c198d1ebd4565223
mamtys <Mamtysman@gmail.com>
2020-04-08 18:22:30 +0300

    fix: fix layout for chrome
    in chrome calc() return float value on division or operation with float numbers.
    So css properties demanding integer values weren't applied.
    Fixed by multiplying ratio by as much as enough to be able to represent it as integers.
    ex. was: '1.5 * 4' now: '6 * 4' coz ratio multiplied by 4

--- src/js/components/Doc/Doc.js
+++ src/js/components/Doc/Doc.js
@@ -3,7 +3,9 @@ import Component from '../../lib/Component';
 class Doc extends Component {
     constructor() {
         super('<div class="virtual-keyboard__doc"></div>');
-        this.node.textContent = 'Keyboard was created at Win OS \n Lang Switch = Shift + LeftAlt';
+        this.node.textContent = `Warning: I didn't noticed, layout is broken in chrome, use firefox to check functionslity
+Keyboard was created at Win OS
+Lang Switch = Shift + LeftAlt`;
     } 
 }
 
